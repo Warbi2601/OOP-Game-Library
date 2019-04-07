0a4e141d394ae44d9c72fe74086d5ec69cae3406
AranWilkinson <34495090+AranWilkinson@users.noreply.github.com>
2019-04-07 22:06:34 +0100

    menu fix

--- OOP - Task 1C/GameMenu.cpp
+++ OOP - Task 1C/GameMenu.cpp
@@ -47,16 +47,7 @@ bool GameMenu::HandleChoice(char choice)
 					Player* player = dynamic_cast<Player*>(app->GetCurrentUser());
 					bool success = player->PurchaseGame(game);
 					cout << success;
-					// Remove cost
-					// game.GetCost();
 				}
-			}
-			else
-			{
-				// this would need to go to a LoginMenu - similar to StoreMenu
-				// instead we just set logged in to true on the main app object
-
-				//app->LogIn();
 			}
 		} break;
 	}
